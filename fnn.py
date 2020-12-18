7a7e26ce89eebd040eff08f3378c28cfe38c3253
Dillon Johnson <dillonj214@gmail.com>
2020-12-18 15:43:19 -0800

    Tweaks to fnn.

--- fnn.py
+++ fnn.py
@@ -27,28 +27,36 @@ def train():
     model = create_model((X_train.shape[1],))
     
     print(model.summary())
-    history = model.fit(X_train, y_train, batch_size=10, epochs=100, verbose=1, validation_split=0.2)
+    history = model.fit(X_train, y_train, batch_size=10, epochs=200, verbose=1, validation_split=0.2)
 
     y_pred = model.predict(X_test)
-    print(r2_score(y_test, y_pred))
-    print(np.sqrt(mean_squared_error(y_test,y_pred)))
-
     performances = data.get_eval_player_performances()
     performances['pred'] = y_pred
 
     rankings = FFEvaluation(performances).get_rankings()
     pd.set_option('display.max_rows', None)
     print(rankings)
+    print('Validation score: {}'.format(r2_score(y_test, y_pred)))
+    print('Validation mean squared error: {}'.format(np.sqrt(mean_squared_error(y_test,y_pred))))
     print('Average rank error: {}'.format(np.mean(abs(rankings['diff']))))
 
     model.save_weights('./fnn_models/fnn')
 
 def test(saved_model):
-    X_test, y_test = FantasyFootballData().get_test_data()
+    data = FantasyFootballData()
+    X_test, y_test = data.get_test_data()
     model = create_model((X_test.shape[1],))
     model.load_weights(saved_model)
     y_pred = model.predict(X_test)
-    print(r2_score(y_test, y_pred))
+
+    performances = data.get_test_player_performances()
+    performances['pred'] = y_pred
+
+    rankings = FFEvaluation(performances).get_rankings()
+    pd.set_option('display.max_rows', None)
+    print(rankings)
+    print('Average rank error: {}'.format(np.mean(abs(rankings['diff']))))
+    print('R2 score: {}'.format(r2_score(y_test, y_pred)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train or test fnn.')
